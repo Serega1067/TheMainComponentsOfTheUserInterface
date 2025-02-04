f6959f13b17e595882cd67c3550b96ac3695e376
Serega1067 <serega1067@yandex.ru>
2025-02-04 14:22:22 +0700

    Init git

---
+++ app/src/main/java/com/example/android_homework/themaincomponentsoftheuserinterface/RadioGroupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_homework.themaincomponentsoftheuserinterface
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RadioGroupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_radio_group)
+    }
+}
