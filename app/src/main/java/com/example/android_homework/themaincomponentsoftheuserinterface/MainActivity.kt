bcf55f99f79b7bb3808a5ca1a4b1539892dd0144
Serega1067 <serega1067@yandex.ru>
2025-02-06 01:43:56 +0700

    Adding activity done

--- app/src/main/java/com/example/android_homework/themaincomponentsoftheuserinterface/MainActivity.kt
+++ app/src/main/java/com/example/android_homework/themaincomponentsoftheuserinterface/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.android_homework.themaincomponentsoftheuserinterface
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android_homework.themaincomponentsoftheuserinterface.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.buttonRadioGroupActivity.setOnClickListener {
+            val intent = Intent(this, RadioGroupActivity::class.java)
+            startActivity(intent)
         }
+        binding.buttonEditTextActivity.setOnClickListener {
+            val intent = Intent(this, EditTextActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonTextInputLayoutActivity.setOnClickListener {
+            val intent = Intent(this, TextInputLayoutActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonChangingTheTheme.setOnClickListener {
+            val intent = Intent(this, ChangingTheTheme::class.java)
+            startActivity(intent)
+        }
+
+        setContentView(binding.root)
     }
 }
